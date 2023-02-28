77781ea9a9922a994a589deb145ed4a2aefc4dc9
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-02-28 22:32:40 +0800

    Update lhmain.py

--- lhmain.py
+++ lhmain.py
@@ -6,7 +6,7 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["pizzaRes"]
 
 # Check if the connection was successful
-if db:
+if db is not None:
     st.write("Connected to MongoDB!")
 else:
     st.write("Failed to connect to MongoDB!")
