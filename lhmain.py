c25f89b6d7d34972378a7db8a6528abfb82ca082
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-02-28 22:33:12 +0800

    Update lhmain.py

--- lhmain.py
+++ lhmain.py
@@ -3,7 +3,7 @@ import pymongo
 
 # Connect to MongoDB
 client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["pizzaRes"]
+db = client["pizzaResss"]
 
 # Check if the connection was successful
 if db is not None:
