83d05f35379875b9a2b8ea995616f54476967186
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-02-28 22:17:06 +0800

    Update lhmain.py

--- lhmain.py
+++ lhmain.py
@@ -7,11 +7,11 @@ password = st.text_input("Enter MongoDB password", type="password")
 database_name = st.text_input("Enter MongoDB database name")
 
 # Connect to MongoDB
-client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.mongodb.net/{database_name}?retryWrites=true&w=majority")
+client = pymongo.MongoClient(f"mongodb+srv://{localhost}:{123456}@cluster0.mongodb.net/{pizzaRes}?retryWrites=true&w=majority")
 db = client[database_name]
 
 # Use the database
-collection = db["collection_name"]
+collection = db["pissa"]
 results = collection.find()
 for result in results:
     st.write(result)
