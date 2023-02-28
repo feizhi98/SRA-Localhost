11bb3257c08cf040a2593d59499b6a9ab1be302a
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-03-01 00:08:45 +0800

    Update lhmain.py

--- lhmain.py
+++ lhmain.py
@@ -1,5 +1,5 @@
 import pymongo
-client = pymongo.MongoClient('mongodb://localhost:3306/')
+client = pymongo.MongoClient('mongodb://localhost:3307/')
 db = client.pizzaRes
 collection = db.pet
 result = collection.find_one({'name': 'John'})
