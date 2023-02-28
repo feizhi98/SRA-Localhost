7e45d21f1a274414c6f13257986c54c1d01a37bc
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-02-28 23:43:19 +0800

    Update lhmain.py

--- lhmain.py
+++ lhmain.py
@@ -1,11 +1,25 @@
+import streamlit as st
 import pymongo
 
-# Connect to the local MongoDB instance
-client = pymongo.MongoClient("mongodb://localhost@localhost:27017/pizzaRes")
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return pymongo.MongoClient(**st.secrets["mongo"])
 
-# Select the database and collection you want to use
-db = client["pizzaRes"]
-collection = db["pissa"]
+client = init_connection()
 
-# Perform some database operations
-result = collection.find_one({"pizza": "durian"})
+# Pull data from the collection.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def get_data():
+    db = client.mydb
+    items = db.mycollection.find()
+    items = list(items)  # make hashable for st.cache_data
+    return items
+
+items = get_data()
+
+# Print results.
+for item in items:
+    st.write(f"{item['name']} has a :{item['pet']}:")
