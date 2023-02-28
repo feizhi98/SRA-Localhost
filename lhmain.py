6e043fa48780ab7cd5753b742433b6232a9e031e
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-02-28 23:17:19 +0800

    Update lhmain.py

--- lhmain.py
+++ lhmain.py
@@ -1,20 +1,11 @@
-import streamlit as st
 import pymongo
 
-# Connect to MongoDB
+# Connect to the local MongoDB instance
 client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+# Select the database and collection you want to use
 db = client["pizzaRes"]
-collection = db["pissa"] # Replace with the name of your collection
+collection = db["pissa"]
 
-# Check if the connection was successful
-if db is not None:
-    st.write("Connected to MongoDB!")
-else:
-    st.write("Failed to connect to MongoDB!")
-
-# Retrieve data from the collection
-data = collection.find()
-
-# Display the data in the Streamlit app
-for pizza in data:
-    st.write(pizza)
+# Perform some database operations
+result = collection.find_one({"pizza": "durian"})
