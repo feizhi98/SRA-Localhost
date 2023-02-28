302f27bce826c4ee6342e24aac94ba5735a163fc
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-02-28 20:01:10 +0800

    Update lhmain.py

--- lhmain.py
+++ lhmain.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pymongo
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["SRA_Project"]
+db = client["pizzaRes"]
 
 # Accept user input
 user_input = st.text_input("Enter data to be stored in MongoDB:")
 
 if st.button("Submit"):
     # Insert the data into MongoDB
-    prediction = db["prediction"]
+    prediction = db["pissa"]
     prediction.insert_one({"data": user_input})
     st.success("Data has been stored in MongoDB!")
