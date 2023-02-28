8c40cd218de435efe39af2b4050d5f04b2c122ef
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-02-28 22:25:22 +0800

    Update lhmain.py

--- lhmain.py
+++ lhmain.py
@@ -1,20 +1,12 @@
 import streamlit as st
 import pymongo
 
-client = pymongo.MongoClient("mongodb://localhost:123456@localhost:27017/pizzaRes")
+# Connect to MongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["pizzaRes"]
-pissa = db["pissa"]
 
-# Accept user input
-user_input = st.text_input("Enter data to be stored in MongoDB:")
-
-if st.button("Submit"):
-    try:
-        # Insert the data into MongoDB
-        pissa.insert_one({"data": user_input})
-        st.success("Data has been stored in MongoDB!")
-    except pymongo.errors.PyMongoError as e:
-        st.error("An error occurred while storing the data in MongoDB:", e)
-    finally:
-        # Close the MongoDB client connection
-        client.close()
+# Check if the connection was successful
+if db:
+    st.write("Connected to MongoDB!")
+else:
+    st.write("Failed to connect to MongoDB!")
