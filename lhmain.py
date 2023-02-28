e20a07273425ec54f39e4c24d7cf722226b58bee
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-02-28 22:37:37 +0800

    Update lhmain.py

--- lhmain.py
+++ lhmain.py
@@ -4,9 +4,17 @@ import pymongo
 # Connect to MongoDB
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["pizzaRes"]
+collection = db["pissa"] # Replace with the name of your collection
 
 # Check if the connection was successful
 if db is not None:
     st.write("Connected to MongoDB!")
 else:
     st.write("Failed to connect to MongoDB!")
+
+# Retrieve data from the collection
+data = collection.find()
+
+# Display the data in the Streamlit app
+for pizza in data:
+    st.write(pizza)
