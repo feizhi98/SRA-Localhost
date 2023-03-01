1594bbe19d4f9c8664f04a66d119cf07827f45e8
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2023-03-01 10:54:04 +0800

    Update lhmain.py

--- lhmain.py
+++ lhmain.py
@@ -1,5 +1,26 @@
 import pymongo
-client = pymongo.MongoClient('mongodb://localhost:27017/pizzaRes')
+import streamlit as st
+
+# Define MongoDB connection string
+MONGODB_CONNECTION_STRING = 'mongodb://localhost:27017'
+
+# Connect to MongoDB and get pet collection
+client = pymongo.MongoClient(MONGODB_CONNECTION_STRING)
 db = client.pizzaRes
 collection = db.pet
-result = collection.find_one({'name': 'John'})
+
+# Define Streamlit interface for adding a pet document
+st.write('Add a new pet:')
+name = st.text_input('Name')
+pet = st.text_input('Pet')
+
+
+# Add pet document to collection when submit button is clicked
+if st.button('Submit'):
+    pet = {
+        'name': name,
+        'pet': pet,
+
+    }
+    collection.insert_one(pet)
+    st.write('Pet added successfully!')
